1b0057126e7943dda7a589941c560bca68de720c
Pablo Ramos <ramos.e.pablo@gmail.com>
2021-01-20 23:55:35 -0500

    restructure structures

--- expression.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type expression struct {
-	operators []operator
-	operands  []operand
-}
--- main.go
+++ main.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type variable struct {
+	letter   string
+	exponent int
+}
+
+type number struct {
+	signedness bool
+	digits     []byte
+	base       byte
+	decimalPos byte
+}
+
+type term struct {
+	coefficient number
+	variables   []variable
+}
+
+type operand struct {
+	constant  *number
+	variable  *variable
+	term      *term
+	operation *operation
+	kind      rune
+}
+
+type operation struct {
+	left     *operand
+	right    *operand
+	operator rune
+}
+
 func main() {
 	fmt.Println("vim-go")
 }
